b9e349a342e7f1dc690159d894197c918ea101e8
Nikhil <jainnikhil7746@gmail.com>
2024-05-24 12:32:44 +0530

    middleware changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 
 const isProtectedRoute = createRouteMatcher([
+  '/',
   '/api/webhooks/clerk',
+  '/api/webhooks/stripe'
   // '/forum(.*)',
 ]);
 
