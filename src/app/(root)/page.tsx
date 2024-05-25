d260f8c5dec4fa1e62742083198b3c99abd245ae
Nikhil <jainnikhil7746@gmail.com>
2024-05-25 12:43:41 +0530

    tagline change

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -14,7 +14,7 @@ const Home = async ({ searchParams }: SearchParamProps) => {
     <>
       <section className="home">
         <h1 className="home-heading">
-          Unleash Your Creative Vision with Glorify
+        Unlock Creativity with Glorify's Design Tools.
         </h1>
         <ul className="flex-center w-full gap-20">
           {navLinks.slice(1, 5).map((link) => (
